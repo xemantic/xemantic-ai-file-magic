7b601f359cb17ebf4f03d4fc5b4e948ac8bb0e91
Kazik Pogoda <morisil@xemantic.com>
2025-01-09 13:05:06 +0100

    updated to the official kotlin code conventions

--- src/commonTest/kotlin/test/MinimalWebP.kt
+++ src/commonTest/kotlin/test/MinimalWebP.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2024 Kazimierz Pogoda / Xemantic
+ * Copyright 2024-2025 Kazimierz Pogoda / Xemantic
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,25 +18,25 @@ package com.xemantic.ai.file.magic.test
 
 @OptIn(ExperimentalUnsignedTypes::class)
 val MINIMAL_WEBP: ByteArray = ubyteArrayOf(
-  // RIFF header
-  0x52u, 0x49u, 0x46u, 0x46u,  // "RIFF"
-  0x1Cu, 0x00u, 0x00u, 0x00u,  // File size minus 8
+    // RIFF header
+    0x52u, 0x49u, 0x46u, 0x46u,  // "RIFF"
+    0x1Cu, 0x00u, 0x00u, 0x00u,  // File size minus 8
 
-  // WEBP header
-  0x57u, 0x45u, 0x42u, 0x50u,  // "WEBP"
+    // WEBP header
+    0x57u, 0x45u, 0x42u, 0x50u,  // "WEBP"
 
-  // VP8L header
-  0x56u, 0x50u, 0x38u, 0x4Cu,  // "VP8L"
-  0x0Eu, 0x00u, 0x00u, 0x00u,  // Chunk size (14 bytes)
+    // VP8L header
+    0x56u, 0x50u, 0x38u, 0x4Cu,  // "VP8L"
+    0x0Eu, 0x00u, 0x00u, 0x00u,  // Chunk size (14 bytes)
 
-  // VP8L stream
-  0x2Fu,                       // Signature byte (0x2F)
-  0x00u,                       // Width-1 (14 bits), Height-1 (14 bits)
-  0x00u,                       // Alpha flag (1 bit), Version (3 bits),
-  0x00u, 0x00u,               // Reserved (3 bits)
+    // VP8L stream
+    0x2Fu,                       // Signature byte (0x2F)
+    0x00u,                       // Width-1 (14 bits), Height-1 (14 bits)
+    0x00u,                       // Alpha flag (1 bit), Version (3 bits),
+    0x00u, 0x00u,               // Reserved (3 bits)
 
-  // Color index (1x1 white pixel)
-  0xFFu, 0xFFu, 0xFFu, 0xFFu,  // RGBA white pixel
+    // Color index (1x1 white pixel)
+    0xFFu, 0xFFu, 0xFFu, 0xFFu,  // RGBA white pixel
 
-  // No need for additional chunks
+    // No need for additional chunks
 ).toByteArray()
